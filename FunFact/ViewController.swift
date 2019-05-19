b4a65c00dd2cacbbdc8ccfad9b038a853dbac858
Vasu Chand <Vasu.chand@mobikwik.com>
2019-05-19 15:04:56 +0530

    initailise projects

---
+++ FunFact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFact
+//
+//  Created by Vasu Chand on 19/05/19.
+//  Copyright © 2019 Vasu Chand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
